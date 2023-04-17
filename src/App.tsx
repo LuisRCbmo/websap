0d953861fb624f5a20ed16d2393a81c3a3252baa
Luis <rosalescaero@gmail.com>
2023-04-17 17:07:04 -0400

    feat:funcion para detectar mora en una fecha
    Se implemento la fecha checkExpireDate en App.tsx, enfuncion de una fecha y la fecha actual se indicara si la fecha expiro, monitoreara si la echa expiro una vez al dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+function checkExpiredDate(billingdate:Date) {
+  const today = new Date();
+  const timeDiffInMs = billingdate.getTime() - today.getTime();
+  const timeDiffInDays = Math.floor(timeDiffInMs / (1000 * 60 * 60 * 24));
+
+  if (timeDiffInDays <= -2) {
+    console.log('La fecha ha expirado');
+  } else {
+    console.log('La fecha aún no ha expirado');
+  }
+}
 
 function App() {
+  const [billingdate, setbillingdate] = useState(new Date('2023-04-17'));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      checkExpiredDate(billingdate);
+    }, 1000);
+    //86400000 milisegundos para monitorear cada 24 horas
+    //1000 milisegundos para pruebas
+
+    return () => clearInterval(interval);
+  }, [billingdate]);
+
   return (
     <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <p>Componente App</p>
     </div>
   );
 }
