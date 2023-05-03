ab2fe8c38297a56b1c0a4dfff11acac07fa43ace
olich797 <olich.castro@gmail.com>
2023-05-02 23:59:22 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import GuardiaFormContainer from './components/UI/organisms/GuardiaFormContainer';
+
 
 function App() {
   return (
     <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <GuardiaFormContainer/>
     </div>
   );
 }
--- src/components/UI/organisms/GuardiaFormContainer.jsx
+++ src/components/UI/organisms/GuardiaFormContainer.jsx
@@ -1,28 +1,24 @@
 import { useState } from 'react';
-import GuardiaForm from './GuardiaForm';
+import GuardiaForm from '../molecules/GuardiaForm';
 
 function GuardiaFormContainer() {
   const [confirmacionVisible, setConfirmacionVisible] = useState(false);
 
   const handleSubmit = async (data) => {
     // Aquí iría el código para guardar los datos en la base de datos
-    // Puedes utilizar una librería como Axios o fetch para enviar una petición al servidor
-
-    // Mostramos el mensaje de confirmación
+   
     setConfirmacionVisible(true);
 
-    // Esperamos un segundo para ocultar el mensaje de confirmación
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    // Ocultamos el mensaje de confirmación
     setConfirmacionVisible(false);
   };
 
   return (
-    <>
+    <div>
       {confirmacionVisible && <p>Guardia asignada correctamente</p>}
       <GuardiaForm onSubmit={handleSubmit} />
-    </>
+      </div>
   );
 }
 
