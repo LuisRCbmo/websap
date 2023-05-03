8ab77b1d79328442c336532ec9d27301b450dc2a
olich797 <olich.castro@gmail.com>
2023-05-02 23:03:25 -0400

    aumento

---
+++ src/components/UI/atoms/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button({ type, children, onClick }) {
+  return (
+    <button type={type} onClick={onClick}>{children}</button>
+  );
+}
+
+export default Button;
