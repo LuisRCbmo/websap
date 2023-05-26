e4b6cd53159c6c1d690ad08bbd96ab8b42662b55
N-th7 <64654131+N-th7@users.noreply.github.com>
2023-05-25 22:01:15 -0400

    solucion app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import PageCobro from './components/pages/PageCobro';
 
 function App() {
   return (
     <div>
-      <header>
-      <PageCobro/>
-      </header>
     </div>
   );
 }
